af787e29c99affd11267716b3b8bb81ad52ff236
timof <timofeykusch@ya.ru>
2021-03-16 00:11:54 +0300

    db 15 03 2021

--- app/src/main/java/com/example/zoorun/SaveRecordActivity.java
+++ app/src/main/java/com/example/zoorun/SaveRecordActivity.java
@@ -3,18 +3,19 @@ package com.example.zoorun;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.view.View;
 import android.widget.*;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SaveRecordActivity extends Activity implements View.OnClickListener {
     private int record;
 
     private TextView tvrecord;
     private EditText editTextName;
-    private Button save;
+    private Button save, openbd, back;
 
     private DBRecords mDBConnector;
     private Context mContext;
@@ -27,7 +28,12 @@ public class SaveRecordActivity extends Activity implements View.OnClickListener
         tvrecord = findViewById(R.id.tvrecord);
         tvrecord.setText(String.valueOf(record));
         editTextName = findViewById(R.id.edit_text_name);
-        save = findViewById(R.id.savetobd);
+        save = findViewById(R.id.savetobdsaveact);
+        save.setOnClickListener(this);
+        openbd = findViewById(R.id.seebasedatabutton);
+        openbd.setOnClickListener(this);
+        back = findViewById(R.id.button3back);
+        back.setOnClickListener(this);
 
         mDBConnector=new DBRecords(this);
         mContext=this;
@@ -48,6 +54,23 @@ public class SaveRecordActivity extends Activity implements View.OnClickListener
 //                Records note = new Records(0, nickname, record); //  Дописать (ID)
                 mDBConnector.insert(nickname, record);
             }
+        }
+        if (v == openbd){
+            setContentView(R.layout.dblist);
+            ArrayList<Records> r = mDBConnector.selectAll();
+            ArrayList<Integer> rec = new ArrayList<>();
+            for (Records record: r){
+                rec.add(record.getValue());
+            }
+            //ДОПИСАТЬ
+            ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, new ArrayList<Integer>(Arrays.asList(1, 5, 4, 2, 4)));
+
+            ListView lv = findViewById(R.id.dblistview);
+            lv.setAdapter(adapter);
+        }
+        if (v == back){
+            Intent intent = new Intent(this, StartActivity.class);
+            startActivity(intent);
         }
     }
 }
